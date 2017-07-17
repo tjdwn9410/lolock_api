0e9c412a44606ed1b23c71de5e43b515cba807c7
Min-ho Kim <minmin0416@naver.com>
2017-07-17 23:27:32 +0900

    Fixing server error5

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -165,10 +165,9 @@ router.post('/loradata', function(req, res, next){
 
   mysql.query("SELECT id FROM lolock_devices WHERE device_id=?",['00000174d02544fffef0103d'])
       .spread(function(rows){
-        console.log(rows[0]);
+        console.log(rows);
         console.log(rows[0].id);
-        var userIdRows = mysql.query("SELECT user_id FROM lolock_register WHERE device_id=?",rows[0].id);
-        console.log(userIdRows);
+        return mysql.query("SELECT user_id FROM lolock_register WHERE device_id=?",rows[0].id)
         /*
         for(var i in userIdRows){
           var gps_rows = mysql.query("SELECT gps_lat, gps_lon FROM lolock_users WHERE id=?", userIdRows[i].user_id)
@@ -177,9 +176,18 @@ router.post('/loradata', function(req, res, next){
         */
 
       })
+      .spread(function(userIdRows){
+        console.log(userIdRows);
+        for(var i in userIdRows){
+          return mysql.query("SELECT gps_lat, gps_lon FROM lolock_users WHERE id=?", userIdRows[i].user_id)
+        }
+      })
+      .spread(function(gpsDataRows){
+        console.log(gpsDataRows);
+      })
 
       // TODO : 경도 위도 user 데이터에서 가져와야함 그리고 등록된 사용자의 출입 기능에서 구현되야함
-      var weatherData_Json = receiveWeatherInfo(126.965255, 37.240982, date, time);
+      var weatherData_Json = JSON.stringify(receiveWeatherInfo(126.965255, 37.240982, date, time));
       //var weatherDataItems = weatherData_Json['response']['body']['items']['item'];   // TODO : 에러 수정
       console.log(weatherData_Json);
       //console.log(weatherDataItems);
