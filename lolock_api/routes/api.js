fb5a04860ba4f082fa68b4fb08a1c5abceeeb781
Min-ho Kim <minmin0416@naver.com>
2017-07-19 20:49:27 +0900

    Fix value name error weathreData -> weatherData

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -346,13 +346,14 @@ var receiveWeatherInfo = function(roomateTokenArray, gps_long, gps_lat, lastModi
   })
 }
 
-var weatherdataModifyRequiredData = function(weathreData, callback) {
+var weatherdataModifyRequiredData = function(weatherData, callback) {
   var POPItem = {}; // 강수 확률  / %로 나옴
   var PTYItem = {}; // 강수 형태  / 0 : 없음 / 1 : 비 / 2: 비/눈 / 3 : 눈
   var SKYItem = {}; // 하늘 상태  / 1 : 맑음 / 2: 구름 조금 / 3: 구름 많음 / 4 : 흐림
   var T3HItem = {}; // 3시간 기온 / 온도로 나옴
-  var weatherDataItemArray = weathreData['response']['body']['items']['item'];
-
+  console.log("weatherData : " + weatherData);
+  var weatherDataItemArray = weatherData['response']['body']['items']['item'];
+  console.log("weatherDataItemArray : " + weatherDataItemArray);
   var data = new Object();
   data.fcstDate = weatherDataItemArray[0].fcstDate;
   data.fcstTime = weatherDataItemArray[0].fcstTime;
