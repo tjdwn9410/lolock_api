9d18e760dae131b028770fedcceb6c676bbaa042
Min-ho Kim <minmin0416@naver.com>
2017-07-19 21:02:39 +0900

    Fix string weatherData to JSON obj1

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -351,8 +351,10 @@ var weatherdataModifyRequiredData = function(weatherData, callback) {
   var PTYItem = {}; // 강수 형태  / 0 : 없음 / 1 : 비 / 2: 비/눈 / 3 : 눈
   var SKYItem = {}; // 하늘 상태  / 1 : 맑음 / 2: 구름 조금 / 3: 구름 많음 / 4 : 흐림
   var T3HItem = {}; // 3시간 기온 / 온도로 나옴
-  console.log("weatherData : " + weatherData);
-  var weatherDataItemArray = weatherData['response']['body']['items']['item'];
+  var weatherDataobj = eval("("+weatherData+")");
+  console.log("weatherDataobj typeof : " + typeof(weatherDataobj));
+  var weatherDataItemArray = weatherDataobj['response']['body']['items']['item'];
+  console.log("weatherDataItemArray typeof : " + typeof(weatherDataItemArray));
   console.log("weatherDataItemArray : " + weatherDataItemArray);
   var data = new Object();
   data.fcstDate = weatherDataItemArray[0].fcstDate;
