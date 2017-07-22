c6bd5da87949faad04ef4260032110367618b45d
Min-ho Kim <minmin0416@naver.com>
2017-07-22 17:17:56 +0900

    Fix error that changed mysql db2

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -312,14 +312,13 @@ router.get('/weatherdata/:LTID', function(req, res, next) {
       console.log(rows[0].id);
       var gps_lat = rows[0].gps_lat;
       var gps_lon = rows[0].gps_lon;
-      return mysql.query("SELECT phone_id FROM lolock_users WHERE id IN (SELECT user_id FROM lolock_register WHERE device_id=?)", rows[0].id);
-    })
-    .spread(function(roomateRows) {
-      var roomateTokenArray = new Array();
-      for (var j in roomateRows) {
-        roomateTokenArray.push(roomateRows[j].phone_id);
-      }
-      receiveWeatherInfo(roomateTokenArray, gps_lon, gps_lat, moment().format('YYYY-MM-DDTHH:mm:ssZ'), 1, res);
+      mysql.query("SELECT phone_id FROM lolock_users WHERE id IN (SELECT user_id FROM lolock_register WHERE device_id=?)", rows[0].id, function(){
+        var roomateTokenArray = new Array();
+        for (var j in roomateRows) {
+          roomateTokenArray.push(roomateRows[j].phone_id);
+        }
+        receiveWeatherInfo(roomateTokenArray, gps_lon, gps_lat, moment().format('YYYY-MM-DDTHH:mm:ssZ'), 1, res);
+      });
     })
 })
 
