70a59765c083838d745664e6b2c9adf0a916b974
Min-ho Kim <minmin0416@naver.com>
2017-07-22 19:20:20 +0900

    Change weatherdata format4

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -519,10 +519,7 @@ var weatherdataModifyRequiredData = function(weatherData, roomateTokenArray, for
   var PTYItem = {}; // 강수 형태  / 0 : 없음 / 1 : 비 / 2: 비/눈 / 3 : 눈
   var SKYItem = {}; // 하늘 상태  / 1 : 맑음 / 2: 구름 조금 / 3: 구름 많음 / 4 : 흐림
   var T1HItem = {}; // 1시간 기온 / 온도로 나옴
-  console.log(moment().format());
-  console.log(moment().format().split('T'));
-  console.log(moment().format().split('T').split(':'));
-  var time = moment().format().split('T').split(':')[0];
+  var time = moment().format().split('T')[1].split(':')[0];
   time += "00";
 
   var weatherDataobj = eval("(" + weatherData + ")");
