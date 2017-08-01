07f97b1fa98b035f76b8c72b821eeb0f28726f4f
Min-ho Kim <minmin0416@naver.com>
2017-08-01 17:28:31 +0900

    checkout checkin 출입자 이름 수정

--- lolock_api/routes/api.js
+++ lolock_api/routes/api.js
@@ -242,16 +242,12 @@ router.get('/checkout/:phone_id', function(req, res, next) {
     var name = "";
 
     mysql.query("UPDATE lolock_users SET flag = 0 WHERE phone_id = ? ", [req.params.phone_id]);
-    mysql.query("SELECT id FROM lolock_users WHERE phone_id=?", req.params.phone_id)
+    mysql.query("SELECT id, name FROM lolock_users WHERE phone_id=?", req.params.phone_id)
         .spread(function(idrows) {
+            name = idrows[0].name;
             return mysql.query("SELECT * FROM lolock_register AS R LEFT JOIN lolock_users AS U ON R.user_id = U.id  WHERE R.device_id = (SELECT device_id FROM lolock_register WHERE user_id = ?)", idrows[0].id)
         })
         .spread(function(roommateRows) {
-            for (var j in roommateRows) {
-              if (roommateRows[j].phone_id == req.params.phone_id){
-                name = roommateRows[j].name;
-              }
-            }
             for (var j in roommateRows) {
                 var pushData = {}
                 if (roommateRows[j].phone_id == req.params.phone_id) {
@@ -313,17 +309,12 @@ router.get('/checkin/:phone_id', function(req, res, next) {
     console.log(req.params.phone_id + "가 들어왔음")
     var name = "";
     mysql.query("UPDATE lolock_users SET flag = 1 WHERE phone_id = ? ", [req.params.phone_id]);
-    mysql.query("SELECT id FROM lolock_users WHERE phone_id=?", req.params.phone_id)
+    mysql.query("SELECT id, name FROM lolock_users WHERE phone_id=?", req.params.phone_id)
         .spread(function(idrows) {
+            name = idrows[0].name;
             return mysql.query("SELECT * FROM lolock_register AS R LEFT JOIN lolock_users AS U ON R.user_id = U.id  WHERE R.device_id = (SELECT device_id FROM lolock_register WHERE user_id = ?)", idrows[0].id)
         })
         .spread(function(roommateRows) {
-          for (var j in roommateRows) {
-            if (roommateRows[j].phone_id == req.params.phone_id){
-              name = roommateRows[j].name;
-            }
-          }
-
             for (var j in roommateRows) {
                 var pushData = {};
                 if (roommateRows[j].phone_id == req.params.phone_id) {
